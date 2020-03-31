279c0336e6b89e0afea4f2e277cef7af05458c9a
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2020-03-31 18:48:12 +0200

    get the data and send them on the socket

--- Server/server.py
+++ Server/server.py
@@ -1,16 +1,23 @@
 import socketio
 import eventlet
+from data_format import DataToSend
 
+# contains the data to send (temperatures, load...)
+myData = DataToSend()
+
+# create the socket
 sio = socketio.Server(cors_allowed_origins='*')
-
 app = socketio.WSGIApp(sio)
 
+# update and send the data every second
 def listen():
     while True:
-        sio.emit('test', 'mydataTest')
+        myData.update()
+        sio.emit('test', myData.toJSON())
         eventlet.sleep(1)
 
 eventlet.spawn(listen)
 
+# listen on localhost => port 80
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 80)), app)
