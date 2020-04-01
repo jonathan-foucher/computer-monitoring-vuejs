a903d678dab4039d91e1a94206c1dc56c4520fa3
JonathanFoucher <46852393+JonathanFoucher@users.noreply.github.com>
2020-04-01 13:22:03 +0200

    create the new VueJS project + chartline with chart.js

---
+++ Client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="pa-0">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  }
+}
+</script>
+
+<style>
+  body {
+    background: #1d2022;
+  }
+  
+  body::-webkit-scrollbar {
+    display: none;
+  }
+</style>
