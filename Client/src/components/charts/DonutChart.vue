11685d47e8551e105e9e4231b39570bc40ce9624
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2020-04-03 14:33:31 +0200

    add text in center of donut charts

--- Client/src/components/charts/DonutChart.vue
+++ Client/src/components/charts/DonutChart.vue
@@ -47,6 +47,49 @@ export default {
       let limit = this.colorsLimits.find(limit => limit.value <= value);
       return limit ? limit.color : 'grey';
     },
+    addCenterTextOnDonutChart() {
+      Chart.pluginService.register({
+        // add the value in the center of the chart
+        beforeDraw: function (chart) {
+          if (chart.config.options.elements.center) {
+            //Get ctx from string
+            var ctx = chart.chart.ctx;
+
+            //Get options from the center object in options
+            var centerConfig = chart.config.options.elements.center;
+            var fontStyle = centerConfig.fontStyle || 'Arial';
+            var txt = centerConfig.text;
+            var color = centerConfig.color || '#000';
+            var sidePadding = centerConfig.sidePadding || 20;
+            var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+            ctx.font = "30px " + fontStyle;
+
+            //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+            var stringWidth = ctx.measureText(txt).width;
+            var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+            // Find out how much the font can grow in width.
+            var widthRatio = elementWidth / stringWidth;
+            var newFontSize = Math.floor(30 * widthRatio);
+            var elementHeight = (chart.innerRadius);
+
+            // Pick a new font size so it will not be larger than the height of label.
+            var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+            //Set font settings to draw it correctly.
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+            var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 1.45);
+            ctx.font = fontSizeToUse + "px " + fontStyle;
+            ctx.fillStyle = color;
+
+            //Draw text in center
+            ctx.fillText(txt, centerX, centerY);
+          }
+        }
+      });
+    },
   },
   mixins: [chartColorsMixin],
   watch: {
@@ -59,11 +102,17 @@ export default {
         this.chart.data.datasets[0].backgroundColor[0] = this.getColorWithAlpha(this[this.getChartColor(newValue)], 0.3);
         this.chart.data.datasets[0].borderColor[0] = this[this.getChartColor(newValue)];
 
+        this.chart.options.elements.center.text = Math.round(newValue) + '%';
+        this.chart.options.elements.center.color = this[this.getChartColor(newValue)];
+        console.log(newValue);
+
         this.chart.update();
       },
     },
   },
   mounted() {
+    this.addCenterTextOnDonutChart();
+
     var ctx = document.getElementById(this.name);
     this.chart = new Chart(ctx, {
       type: 'doughnut',
@@ -86,6 +135,14 @@ export default {
         ]
       },
       options: {
+        elements: {
+          center: {
+            text: Math.round(this.dataset) + '%',
+            color: this[this.getChartColor(this.dataset)],
+            fontStyle: 'Helvetica',
+            sidePadding: 40,
+          },
+        },
         circumference: Math.PI,
         rotation: Math.PI,
         title: {
