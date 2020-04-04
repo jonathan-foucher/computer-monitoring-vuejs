78f7c26c18c0816aa894b91f18bf41b7cf4fe4e1
JonathanFoucher <46852393+JonathanFoucher@users.noreply.github.com>
2020-04-04 08:53:37 +0200

    create a new component for the SSD/HDD bars

---
+++ Client/src/components/DisksComponent.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+  <span class="graph-title">
+    SSD/HDD used space %
+  </span>
+  <br>
+
+  <v-progress-linear
+    background-opacity="0.3"
+    :background-color="grey"
+    height="35px"
+    :rounded="true"
+    :value="ssd1UsedSpace"
+    :color="getDiskColor(ssd1UsedSpace)">
+      <span class="text-progress-bar">
+        {{ ssd1Name }} ({{ Math.round(ssd1UsedSpace) }} %)
+      </span>
+    </v-progress-linear>
+  <br>
+
+  <v-progress-linear
+    background-opacity="0.3"
+    :background-color="grey"
+    height="35px"
+    :rounded="true"
+    :value="ssd2UsedSpace"
+    :color="getDiskColor(ssd2UsedSpace)">
+      <span class="text-progress-bar">
+        {{ ssd2Name }} ({{ Math.round(ssd2UsedSpace) }} %)
+      </span>
+    </v-progress-linear>
+  <br>
+
+  <v-progress-linear
+    background-opacity="0.3"
+    :background-color="grey"
+    height="35px"
+    :rounded="true"
+    :value="hdd1UsedSpace"
+    :color="getDiskColor(hdd1UsedSpace)">
+      <span class="text-progress-bar">
+        {{ hddd1Name }} ({{ Math.round(hdd1UsedSpace) }} %)
+      </span>
+  </v-progress-linear>
+</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import chartColorsMixin from '../mixins/chartColorsMixin';
+
+export default {
+  mixins: [chartColorsMixin],
+  computed: {
+    ...mapState([
+      'ssd1Name',
+      'ssd1UsedSpace',
+      'ssd2Name',
+      'ssd2UsedSpace',
+      'hddd1Name',
+      'hdd1UsedSpace',
+    ]),
+  },
+  methods: {
+    getDiskColor(value) {
+      if(value >= 90)
+        return this.getColorWithAlpha(this.red, 0.9);
+
+      else if(value >= 75)
+        return this.getColorWithAlpha(this.orange, 0.9);
+
+      else if(value >= 60)
+        return this.getColorWithAlpha(this.yellow, 0.9);
+
+      return this.getColorWithAlpha(this.green, 0.9);
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .text-progress-bar {
+    font-weight: 1000;
+  }
+
+  .graph-title {
+    color: rgb(20, 150, 230);
+    font-weight: bold;
+    padding: 10px;
+    font-size: 20px;
+    font-family: 'Arial';
+    display: block;
+    text-align: center;
+  }
+</style>
